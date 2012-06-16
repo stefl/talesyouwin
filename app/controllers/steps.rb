beb32c6353f30dd885c3255ea3589d2e8365a731
Stef Lewandowski <stef@stef.io>
2012-06-16 22:45:50 +0100

    buttons

--- app/controllers/steps.rb
+++ app/controllers/steps.rb
@@ -3,16 +3,21 @@ TalesYouWin.controllers :steps do
   get :show, :map => "/tales/:tale_slug/steps/:slug" do
     not_found unless @tale = Tale.where(:slug => params[:tale_slug]).first
     not_found unless @step = @tale.steps.where(:slug => params[:slug]).first
-    puts "CREATE STEP"
-    puts session[:tales].inspect
     set_current_step @step
-    puts session[:tales].inspect
     @next_step = @tale.steps.where(:ordering => @step.ordering + 1).first
     render :"steps/show"
+  end
+
+  get :edit, :map => "/tales/:tale_slug/steps/:slug/edit" do
+    not_found unless @tale = Tale.where(:slug => params[:tale_slug]).first
+    not_found unless @step = @tale.steps.where(:slug => params[:slug]).first
+    halt(403, "Not Authorized") unless @step.tale.account == current_account
+    render :"steps/edit"
   end
   
   post :create, :map => "/tales/:slug/steps" do
     not_found unless @tale = Tale.where(:slug => params[:slug]).first
+    halt(403, "Not Authorized") unless @tale.account == current_account
     @step = Step.new(params[:step])
     @step.tale = @tale
     @step.ordering = @tale.steps.max(:ordering) + 1
--- app/controllers/tales.rb
+++ app/controllers/tales.rb
@@ -17,6 +17,11 @@ TalesYouWin.controllers :tales do
     render :'tales/edit'
   end
 
+  get :manage, :map => "/tales/:slug/manage" do
+    not_found unless @tale = Tale.where(:slug => params[:slug]).first
+    render :'tales/manage'
+  end
+
   get :play, :map => "/tales/:slug/play" do
     not_found unless @tale = Tale.where(:slug => params[:slug]).first
     redirect url_for(:steps, :show, :tale_slug => @tale.slug, :slug => current_step(@tale).slug)
