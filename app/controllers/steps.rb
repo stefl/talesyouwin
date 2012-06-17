863a6f20020df07f677ab276184ea18b137aa78f
Stef Lewandowski <stef@stef.io>
2012-06-17 11:20:32 +0100

    editing

--- app/controllers/steps.rb
+++ app/controllers/steps.rb
@@ -11,13 +11,20 @@ TalesYouWin.controllers :steps do
   get :edit, :map => "/tales/:tale_slug/steps/:slug/edit" do
     not_found unless @tale = Tale.where(:slug => params[:tale_slug]).first
     not_found unless @step = @tale.steps.where(:slug => params[:slug]).first
-    halt(403, "Not Authorized") unless @step.tale.account == current_account
+    halt(403, "Not Authorized") unless @step.tale.account.id == current_account.id
     render :"steps/edit"
+  end
+
+  put :update, :map => "/tales/:tale_slug/steps/:slug/" do
+    not_found unless @tale = Tale.where(:slug => params[:tale_slug]).first
+    not_found unless @step = @tale.steps.where(:slug => params[:slug]).first
+    @step.update_attributes(params[:step])
+    redirect url_for(:tales, :manage, :slug => @tale.slug)
   end
   
   post :create, :map => "/tales/:slug/steps" do
     not_found unless @tale = Tale.where(:slug => params[:slug]).first
-    halt(403, "Not Authorized") unless @tale.account == current_account
+    halt(403, "Not Authorized") unless @tale.account.id == current_account.id
     @step = Step.new(params[:step])
     @step.tale = @tale
     @step.ordering = (@tale.steps.max(:ordering) || 0) + 1
